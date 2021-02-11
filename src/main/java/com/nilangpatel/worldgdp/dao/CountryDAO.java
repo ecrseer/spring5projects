cc66bb1c08210801bc4b1b7e94be8bfceb927aaa
ecrseer <juustino@live.com>
2021-02-11 20:16:47 -0300

    methd required;

--- src/main/java/com/nilangpatel/worldgdp/dao/CountryDAO.java
+++ src/main/java/com/nilangpatel/worldgdp/dao/CountryDAO.java
@@ -6,7 +6,21 @@ import java.util.List;
 import java.util.Map;
 
 public class CountryDAO {
+    private String SELECT_FROM;
+    private int PAGE_SIZE=0;
+
     public List<Country> getCountries(Map<String,Object> parametros){
+        int nPaginas=0;
+        if(parametros.containsKey("PageNo"))
+            nPaginas = Integer.parseInt(parametros.get("PageNo")
+                    .toString());
+
+        Integer offset = (nPaginas -1)*PAGE_SIZE;
+        parametros.put("offset",offset);
+        parametros.put("size",PAGE_SIZE);
+
+
+
         return null;
     }
 }
